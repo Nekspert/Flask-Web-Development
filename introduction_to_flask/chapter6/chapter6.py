4a50b7946c21df1db4e69f65e2858e5b814f38a3
Timur Berezin <timur.livepart1@gmail.com>
2025-05-10 09:17:24 +0400

    Feat: Chapter 6. Email
    Added Sending Asynchronous Email:
    If you sent a few test emails, you likely noticed that the mail.send() function blocks for a few seconds
    while the email is sent, making the browser look unresponsive during that time. To avoid unnecessary
    delays during request handling, the email send function can be moved to a background thread.

---
+++ introduction_to_flask/chapter6/chapter6.py
@@ -0,0 +1,103 @@
+import os
+from threading import Thread
+
+from dotenv import load_dotenv
+from flask import Flask, render_template, session, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail, Message
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+load_dotenv()
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data2.sqlite')
+app.config['SECRET_KEY'] = 'chapter6'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = os.environ.get('FLASKY_MAIL_SENDER')
+
+mail = Mail(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db, directory=os.path.join(os.path.dirname(__file__), 'migrations'))
+bootstrap = Bootstrap(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField(label='What is your name?', validators=[DataRequired()])
+    submit = SubmitField(label='Submit')
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return f'<Role "{self.name}">'
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return f'<User "{self.username}">'
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
+
+def send_async_mail(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(to, subject, template, **kwargs) -> Thread:
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_mail, args=(app, msg))
+    thr.start()
+    return thr
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User().query.filter_by(username=form.name.data).first()
+        if user is None:
+            print(1)
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                print(2)
+                send_email(app.config['FLASKY_ADMIN'], ' New User', 'mail/new_user', user=user)
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form,
+                           name=session.get('name'), known=session.get('known', False))
