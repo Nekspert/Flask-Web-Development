f740ecaae0cc47aed4f778f1c5124819f311695c
Timur Berezin <timur.livepart1@gmail.com>
2025-05-16 22:59:59 +0400

    Feat: Chapter 8. User Authentication
    Added migrations, .env.example file, new libs for validation,
    
    Added tests: basic tests and user model tests
    
    Added new fields to database. New methods. Added new migrations from my side to new sqlalchemy orm version
    
    Added login, register, reset password, change password, change email mechanisms

---
+++ Flasky/app/__init__.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+from config import config
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+
+
+def create_app(config_name) -> Flask:
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    # attach routes and custom error pages here
+    from .main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    from .auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    return app
